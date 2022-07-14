d59197a62326399f988f4cdaa6d49e8de0b2bbec
PhilipDW183 <philip.davidwilkinson@gmail.com>
2022-07-14 10:20:26 +0100

    Refactor existing tests to use fixtures

--- tests/data/test_view_model.py
+++ tests/data/test_view_model.py
@@ -1,27 +1,10 @@
 from todo_app.data.view_model import ViewModel
 from todo_app.data.trello_items import Item
+import pytest
 
 
-def test_doing_items_property():
-
-    item1 = Item("id1", "test task 1", "1234", "test task desc", "16-07-2022")
-    item2 = Item("id2", "test task 2", "12345", "test task desc", "15-07-2022")
-    item3 = Item("id3", "test task 3", "123456", "test task desc", "15-07-2022")
-
-    items = [item1, item2, item3]
-
-    list_targets = {"To Do": "1234",
-                    "Doing": "12345",
-                    "Done": "123456"}
-
-    view_model = ViewModel(items, list_targets)
-
-    doing_items = view_model.doing_items
-
-    assert doing_items == [item2]
-
-
-def test_to_do_items_property():
+@pytest.fixture
+def items_fixture():
 
     item1 = Item("id1", "test task 1", "1234", "test task desc", "16-07-2022")
     item2 = Item("id2", "test task 2", "12345", "test task desc", "15-07-2022")
@@ -29,12 +12,36 @@ def test_to_do_items_property():
 
     items = [item1, item2, item3]
 
+    return items
+
+
+@pytest.fixture
+def list_targets_fixture():
+
     list_targets = {"To Do": "1234",
                     "Doing": "12345",
                     "Done": "123456"}
 
-    view_model = ViewModel(items, list_targets)
+    return list_targets
 
-    to_do_items = view_model.to_do_items
 
-    assert to_do_items == [item1]
+@pytest.fixture
+def view_model_fixture(items_fixture, list_targets_fixture):
+
+    view_model = ViewModel(items_fixture, list_targets_fixture)
+
+    return view_model
+
+
+def test_to_do_items_property(items_fixture, view_model_fixture):
+
+    to_do_items = view_model_fixture.to_do_items
+
+    assert to_do_items == [items_fixture[0]]
+
+
+def test_doing_items_property(items_fixture, view_model_fixture):
+
+    doing_items = view_model_fixture.doing_items
+
+    assert doing_items == [items_fixture[1]]
