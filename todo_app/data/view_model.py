c7491b77cb2e3095d28db75587f5d49cf4f2fbc1
PhilipDW183 <philip.davidwilkinson@gmail.com>
2022-07-14 17:06:26 +0100

    Create item filter func in ViewModel

--- todo_app/data/view_model.py
+++ todo_app/data/view_model.py
@@ -15,17 +15,20 @@ class ViewModel:
     @property
     def to_do_items(self):
         to_do_items_id = self.list_targets.get("To Do")
-        to_do_items = [item for item in self.items if item.idList == to_do_items_id]
+        to_do_items = self.filter_items(self.items, to_do_items_id)
         return to_do_items
 
     @property
     def doing_items(self):
         doing_items_id = self.list_targets.get("Doing")
-        doing_items = [item for item in self.items if item.idList == doing_items_id]
+        doing_items = self.filter_items(self.items, doing_items_id)
         return doing_items
 
     @property
     def done_items(self):
         done_items_id = self.list_targets.get("Done")
-        done_items = [item for item in self.items if item.idList == done_items_id]
+        done_items = self.filter_items(self.items, done_items_id)
         return done_items
+
+    def filter_items(self, items, id):
+        return [item for item in items if item.idList == id]
